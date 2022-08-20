9df4e4f361c26f304d5d06356120086612e68343
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 17:23:07 +0300

    .

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(cors());
 
 app.use(express.static("client", "build"));
 
-app.get("*", (req, res) => {
-res.sendFile(path.resolve(__dirname, "client",  "build", "index.html"));
-});
+app.use("/", (req, res) => {
+    res.send("RUnnig")
+})
 
 const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
