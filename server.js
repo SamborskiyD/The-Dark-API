4ab3175f61e9491b5c3fc22c6d136ed65421541c
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 16:57:19 +0300

    .

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use("/", express.static(path.join(__dirname, "client", "build")))
 
-app.get("/",  (req, res) => {
+app.get("/app",  (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 })
 
