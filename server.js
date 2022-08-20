09cbe720216c309d01225d189ee81e5d2cc2fa54
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 23:34:23 +0300

    ,

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 
-
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get('/', function (req, res) {
     const index = path.join(__dirname, "client", 'build', 'index.html');
