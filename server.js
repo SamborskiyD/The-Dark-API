39d5b80006031734bc05397e108fd8a6143118f4
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 23:06:47 +0300

    .

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 
-app.use("/", express.static(path.join(__dirname, "client", "build")));
+app.use(express.static("app/client/build"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "app/client/build", "index.html"));
 });
 
 
