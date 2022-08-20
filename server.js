675a9b8cf41f1a16a531bbae5eed340beaaf4e76
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 15:52:00 +0300

    .

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*",  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
+
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+.then(() => {app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });})
 .catch((err) => console.log(err.message));
 
@@ -37,10 +45,3 @@ app.use("/api", CharacterRoute);
 app.use("/api", EpisodRoute);
 app.use("/api", ImageRoute);
 app.use("/api/character/avatar/image", express.static("images"))
-
-
-app.use("/", express.static(path.join(__dirname, "client", "build")))
-
-app.get("*",  (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-})
