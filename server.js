ed6c1162f898cbbd71faae5a77090c27f815ffd9
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 15:24:59 +0300

    .

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*",  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
+
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {app.listen(process.env.PORT || 5000, () => {
@@ -39,8 +46,3 @@ app.use("/api", ImageRoute);
 app.use("/api/character/avatar/image", express.static("images"))
 
 
-app.use("/", express.static(path.join(__dirname, "client", "build")))
-
-app.get("*",  (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-})
