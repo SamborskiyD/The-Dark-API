5bb62363d99e2c10860029b8aa6fc60c671920e2
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 17:11:28 +0300

    .

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("/app",  (req, res) => {
+app.get("/database",  (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 })
 
 const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {app.listen(PORT || 5000, () => {
+.then(() => {app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });})
 .catch((err) => console.log(err.message));
