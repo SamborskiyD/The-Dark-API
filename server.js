563aeeb9f87a315dc274925e3b4e3bfe9ee65cb4
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-21 12:43:26 +0300

    ,

--- server.js
+++ server.js
@@ -16,13 +16,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-
-app.use(express.static("client/build"));
-
-app.get('/', function (req, res) {
-    const index = path.join("./client/build", 'index.html');
-    res.sendFile(index);
-  });
+const PORT = process.env.PORT || 5000
 
 app.get("/api", (req, res) => {
     res.json({
@@ -31,17 +25,21 @@ app.get("/api", (req, res) => {
     })
 })
 
-
 app.use("/api", CharacterRoute);
 app.use("/api", EpisodRoute);
 app.use("/api", ImageRoute);
 app.use("/api/character/avatar/image", express.static("images"))
 
 
-const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});})
-.catch((err) => console.log(err.message));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
